1b8cb6f2be9cdec43df41ce39254bb908e55a136
Vlai22 <cherkovvald@gmail.com>
2025-12-17 14:25:14 +0400

    Initial commit

---
+++ src/components/UI/Button.vue
@@ -0,0 +1,37 @@
+<template>
+    <button :style="width + height">{{ text }}</button>
+</template>
+<script>
+export default{
+    props:{
+        text:{
+            type: String,
+            default: "Clock Here!"
+        },        
+        width:{
+            type: String,
+            default: 'width :496px;'
+        },
+        height:{
+            type: String,
+            default: 'height: 44px;'
+        }
+    },
+}
+</script>
+<style scoped>
+button{
+    padding: 10px 28px;
+    margin-top: 25px;
+    border-radius: 5px;
+    background-color: #8B5CF6;
+    color: white;
+    outline: none;
+    border: none;
+    font-size: 16px;
+    transition: all 0.5s;
+}
+button:hover{
+    background-color: #6D28D9;
+}
+</style>
